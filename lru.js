c66d9562cfaa50fd9b01783c51520006064a6fa2
Hank Jackson <hjackson277@gmail.com>
2022-06-14 17:05:12 -0400

    added get functionality to lru

--- lru.js
+++ lru.js
@@ -1,7 +1,8 @@
-function Node (value, next) {
+function Node (value, next, key) {
   this.next = next;
   this.prev = null;
   this.value = value;
+  this.key = key; // added this to delete the value from the cache
 }
 
 class DoublyLinkedList {
@@ -10,68 +11,147 @@ class DoublyLinkedList {
     this.tail = null;
   }
 
-  addHead (value) {
-    // const node = new Node(value);
-    this.head = new Node(value, this.head);
+  addHead (value, key) {
+    this.head = new Node(value, this.head, key);
+  
+    if (!this.tail) this.tail = this.head;
+    else this.head.next.prev = this.head;
     
-    if (!this.tail) {
-      this.tail = this.head;
-      // return node;
-    }
-    else {
-      this.head.next.prev = this.head;
-    }
     return this.head;
-    
-    // node.next = this.head;
-    // this.head.prev = node;
-    // this.head = node;
-    // return node;
   }
 
   deleteTail () {
-    this.tail = this.tail.prev;
-    this.tail.next = null;
+    const deleted = this.tail; // what if head is null? it returns null?
+    if (this.head === this.tail) this.head = this.tail = null;
+    else {
+      this.tail = this.tail.prev
+      this.tail.next = null;
+    }
+    return deleted;
+  }
+
+  deleteHead () {
+    const deleted = this.head; // what if head is null? it returns null?
+    if (this.head === this.tail) this.head = this.tail = null;
+    else {
+      this.head = this.head.next
+      this.head.prev = null;
+    }
+    return deleted;
   }
   
-  move () {
+  // moveNode (node) {
+  //   const OGprevNode = node.prev;
 
-  }
+  //   //Bypass the current node in the original linkedlist
+  //   node.prev.next = node.next;
+  //   node.next.prev = OGprevNode;
+  //   node.next = null;
+  //   node.prev = null;
+
+  //   return this.addHead(node.value, node.key);
+  // }
 
   printList() {
-    let current = this.head;
+    let current = this.tail;
     while(current) {
-      console.log(current.value)
-      current = current.next;
+      console.log(current.key + ': ' + current.value)
+      current = current.prev;
     }
   }
 }
-
 
 class LRU {
   constructor () {
     this.list = new DoublyLinkedList();
     this.map = {};
     this.length = 0;
-    this.capacity = 10;
+    this.capacity = 5;
   }
 
   put (key, value) {
-    this.map[key] = this.list.addHead(value);
-    this.length++;
-    if (this.capacity >= this.length){
-      this.list.deleteTail();
+    // should we be moving replaced key/value pair to the head of the list?
+    if (this.map[key]) {
+      const node = this.map[key];
+
+      if (this.list.head === node) this.list.deleteHead();
+      else if (this.list.tail === node) this.list.deleteTail();
+      else {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+      }
+      --this.length;
     }
+
+    this.map[key] = this.list.addHead(value, key);
+    if (this.length < this.capacity) this.length++;
+    else {
+      const deletedNode = this.list.deleteTail();
+      delete this.map[deletedNode.key];
+    }
+    return this.map[key];
   }
+
 
   get (key) {
+    if (this.map[key]) {
+      const node = this.map[key];
 
+      if (this.list.head === node) return node.value;
+      else if (this.list.tail === node) this.list.deleteTail();
+      else {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
+        node.next = null;
+        node.prev = null;
+      }
+      return this.list.addHead(node.value, node.key).value;
+    } 
   }
+
+
+  // get (key) {
+  //   //See if key exists in our current map
+  //   if(this.map[key]){
+  //     //Also need to move this node to the front/head of the linked list.
+  //     this.list.moveNode(this.map[key]);
+  //     //Return the value
+  //     return this.map[key].value;
+  //   } //If not, return undefined, continue on with the user's middleware chain, and save the result via the put function AFTER
+  //   //the user finishes their middleware stuff
+  //   else {
+  //    return undefined;
+  //   }
+  // }
+
+  printLru() {
+    console.log('LIST')
+    this.list.printList();
+    console.log('\n')
+    //console.log('CACHE')
+    //for(const i in this.map) console.log(i + ': ' + this.map[i].value);
+  }
+
 }
 
-const dbl = new DoublyLinkedList();
-dbl.addHead(3);
-dbl.addHead(5);
-dbl.addHead(7);
-// console.log(dbl)
-dbl.printList();
+const lru = new LRU();
+lru.put('a', 1)
+lru.put('b', 2)
+lru.put('c', 3)
+lru.put('d', 4)
+lru.put('b', 5)
+lru.put('e', 7)
+lru.put('c', 10)
+lru.put('d', 11)
+lru.put('d', 12)
+lru.put('f', 15)
+lru.put('d', 11)
+
+lru.printLru();
+lru.get('e')
+lru.printLru();
+console.log('length', lru.length)
+
+
+// when evicting a key from the cache, remove the 
