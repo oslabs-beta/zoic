c8911610a528d7b1c4ab76bdf3590cc88c50b587
Hank Jackson <hjackson277@gmail.com>
2022-06-15 11:54:17 -0400

    restructured folders

---
+++ mockup.ts
@@ -0,0 +1,10 @@
+// import { LRU, redisCache, LFU } from 'asdf.com';
+
+// const lru = new LRU({
+//   returnOnHit: true,
+//   timeInCache: 200, //???
+// });
+
+// router.get('/readJson', lru.get, conroller.queryDB, lru.put, ctx => {
+//   ctx.response.body = ctx.state.json;
+// });
