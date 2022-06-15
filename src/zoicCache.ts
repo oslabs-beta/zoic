e760cf311e5e67b9daad198859a0883792ce4871
Hank Jackson <hjackson277@gmail.com>
2022-06-15 14:23:07 -0400

    added comments for mock up middleware

--- src/zoicCache.ts
+++ src/zoicCache.ts
@@ -18,3 +18,24 @@ lru.printLru();
 lru.get('e')
 lru.printLru();
 console.log('length', lru.length)
+
+
+//define middleware method/function, taking in options as arg
+//code example:
+//this.middleware = function (strDuration, middlewareToggle, localOptions)
+  //define helper function, 'cache', within middleware method for HTTP route handling, taking in req, res, and next
+  //code exmple:
+  //var cache = function(req, res, next)
+    //assign local variable 'key' the value of req.originalURL or req.url (will be different Oak)
+    //attempt cache hit, assigning variable 'cached' the value of calling the imported cache function's get method, passing in 'key'
+    //if 'cached' has value
+      //return the the value of calling 'sendCachedResponse' passing in, req, res, 'cached', next, etc (function definition requires further investigation)
+        //code example:
+        // perf.hit(key)
+        //return sendCachedResponse(req, res, cached, middlewareToggle, next, duration)
+    //if 'cached' does NOT have value
+      //return value of calling 'makeResponseCachable' passing in, req, res, 'cached', next, etc (function definition requires further investigation)
+        //code example:
+        // perf.miss(key)
+        //return makeResponseCacheable(req, res, next, key, duration, strDuration, middlewareToggle)
+  //middleware function returns value of calling 'cache'
