ba7248bce78a7a7cc38c5256e1ab89a816c3fe76
jnarvil3 <jaspernarvil@gmail.com>
2022-06-16 13:18:43 -0300

    pls save

--- test_server/router.ts
+++ test_server/router.ts
@@ -1,17 +1,11 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import controller from './controllers.ts';
-import LRU from '../src/lru.js';
+import  { ZoicCache } from '../src/zoicCache.ts';
 
 const router = new Router();
-const lru = new LRU();
+const cache = new ZoicCache({ cache: 'LRU' });
 
-const cache = new zoicCache({
-  cacheType: LRU,
-  returnOnHit: true,
-  time: 10
-});
-
-router.get('/readJson', lru.get, controller.jsonRead, lru.put, ctx => {
+router.get('/readJson', cache.get, controller.jsonRead, cache.put, ctx => {
     console.log('ctx.response.body: ', ctx.state.json)
     ctx.response.body = ctx.state.json;
 });
