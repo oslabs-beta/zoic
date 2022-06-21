8bbef2df8d822f58b20f74b871341f9122735547
Hank Jackson <hjackson277@gmail.com>
2022-06-21 17:01:56 -0400

    restructured test files

--- src/fetchTest.ts
+++ src/fetchTest.ts
@@ -1,10 +1,8 @@
 export const test = async () => {
   for (let i = 1; i < 5000; i++){
-
     const res = await fetch(`http://localhost:8000/dbRead/${Math.floor(Math.random() * 100)}`);
     const body = await res.json();
     console.log(body);
-
   }
 }
 
