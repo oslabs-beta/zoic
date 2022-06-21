addc7d9c42792db0a95a34561d01d0ca50692424
Hank Jackson <hjackson277@gmail.com>
2022-06-21 10:39:17 -0400

    deleted some random extra stuff in db model

--- model/db.ts
+++ model/db.ts
@@ -1,18 +1,9 @@
 import { Client } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-// import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
 
 const config = Deno.env.get("SWARS_API_KEY");
 
 const client = new Client("postgres://jwzjcurw:qKN9rE7SRHSYnAXAgeDjUOlocAScPv74@drona.db.elephantsql.com/jwzjcurw");
 await client.connect();
 
-// try {
-//   const result = await client.queryArray(`SELECT * FROM "public"."films" LIMIT 10`);
-//   console.log(result);
-// } catch {
-//   console.log('error during connection')
-// }
-
-// await client.end();
 
 export default client;
