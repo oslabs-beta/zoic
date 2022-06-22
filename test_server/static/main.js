47b051fc9af9a939413a778ab54f89ca899d07ca
Hank Jackson <hjackson277@gmail.com>
2022-06-22 14:50:44 -0400

    cleaned up front end js file more

--- test_server/static/main.js
+++ test_server/static/main.js
@@ -6,16 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const entries = document.createElement('p');
   const avMissLatency = document.createElement('p');
   const avHitLatency = document.createElement('p');
+
   root.appendChild(hits);
   root.appendChild(misses);
   root.appendChild(entries);
   root.appendChild(avMissLatency);
   root.appendChild(avHitLatency);
-  entries.innerHTML = "Number of entries: 0";
-  hits.innerHTML = "Reads processed: 0";
-  misses.innerHTML = "Writes processed: 0";
-  avHitLatency.innerHTML = "Average cache hit latency: 0ms";
-  avMissLatency.innerHTML = "Average cache miss latency: 0ms";
+
+  entries.innerHTML = "Number of entries: loading...";
+  hits.innerHTML = "Reads processed: loading...";
+  misses.innerHTML = "Writes processed: loading...";
+  avHitLatency.innerHTML = "Average cache hit latency: loading...";
+  avMissLatency.innerHTML = "Average cache miss latency: loading...";
   
   
   setInterval(() => {
@@ -24,21 +26,22 @@ document.addEventListener('DOMContentLoaded', () => {
       'Cache-Control': "no-cache"},
       'pragma': 'no-cache'})
     .then(response => response.json())
-    .then(obj => {
+    .then(metricsData => {
       
-      const {number_of_entries, reads_processed, writes_processed, average_hit_latency, average_miss_latency} = obj;
+      const {
+        number_of_entries,
+        reads_processed,
+        writes_processed,
+        average_hit_latency,
+        average_miss_latency
+      } = metricsData;
     
       entries.innerHTML = `Number of entries: ${number_of_entries || '0'}`;
-    
       hits.innerHTML = `Reads processed: ${reads_processed || '0'}`;
-    
       misses.innerHTML = `Writes processed: ${writes_processed || '0'}`;
-
       avHitLatency.innerHTML = `Average cache hit latency: ${average_hit_latency || '0'}ms`;
-
-      avMissLatency.innerHTML = `Average cache miss latency: ${average_miss_latency || '0'}ms`;
-
-    
+      avMissLatency.innerHTML = `Average cache miss latency: ${average_miss_latency || '0'}ms`
+  
   })}, 1500);
 
 });
